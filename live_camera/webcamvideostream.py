86f4bd187698e7f98c9534b1245d7f4ad51c644e
Ergülü Bozkurt <ergulubozkurt5@gmail.com>
2025-05-09 16:24:27 +0300

    Update webcamvideostream.py

--- live_camera/webcamvideostream.py
+++ live_camera/webcamvideostream.py
@@ -1,32 +1,30 @@
-import cv2
+from picamera2 import Picamera2
 from threading import Thread
+import numpy as np
 
 class WebcamVideoStream:
-    def __init__(self, src = 0):
+    def __init__(self):
         print("init")
-        self.stream = cv2.VideoCapture(src)
-        (self.grabbed, self.frame) = self.stream.read()
+        self.picam2 = Picamera2()
+        self.picam2.configure(self.picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+        self.picam2.start()
+        self.frame = None
         self.stopped = False
-        
-    
+
     def start(self):
         print("start thread")
         t = Thread(target=self.update, args=())
         t.daemon = True
         t.start()
         return self
-    
+
     def update(self):
         print("read")
-        while True:
-            if self.stopped:
-                return
-            
-            (self.grabbed, self.frame) = self.stream.read()
-    
+        while not self.stopped:
+            self.frame = self.picam2.capture_array()
+
     def read(self):
         return self.frame
-    
+
     def stop(self):
         self.stopped = True
-
